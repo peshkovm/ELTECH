eec10a1770a6168428500cfe118c18c433b0b94a
Denis Peshkov <denis_maksim@mail.ru>
2018-09-13 14:13:08 +0300

    Restructured the program? added new classes

--- AI labs/AI_labs/src/com/eltech/lab1/Game8.java
+++ AI labs/AI_labs/src/com/eltech/lab1/Game8.java
@@ -1,36 +1,51 @@
 package com.eltech.lab1;
 
-import java.util.Collection;
-import java.util.Queue;
+import java.util.*;
 
-public class Game8 extends GraphSearch<Integer[][], String> {
-
-    private static class Matrice{
-
+class Matrix {
+    Matrix(Integer[][] mas, int x, int y) {
+        this.mas = mas;
+        this.x = x;
+        this.y = y;
     }
 
-    public Game8(Queue<Node<Integer[][], String>> fringe) {
+    Integer[][] mas;
+    int x;
+    int y;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Matrix) {
+            Matrix temp = (Matrix) obj;
+            if (Arrays.deepEquals(mas, temp.mas) && x == temp.x && y == temp.y)
+                return true;
+        }
+        return false;
+    }
+}
+
+public class Game8 extends GraphSearch<Matrix, String> {
+
+    List<Integer> a = new ArrayList<Integer>();
+
+    public Game8(Queue<Node<Matrix, String>> fringe) {
         super(fringe);
     }
 
     @Override
-    protected Node<Integer[][], String> Solution(Node<Integer[][], String> node) {
+    protected Node<Matrix, String> Solution(Node<Matrix, String> node) {
         return node;
     }
 
     @Override
-    protected boolean Goal_Test(Integer[][] state) {
-        if (state.equals(new Integer[][]{
-                {1, 2, 3},
-                {null, 4, 5},
-                {6, 7, 8}
-        }))
+    protected boolean Goal_Test(Matrix state) {
+        if (state.equals(new Matrix(new Integer[][]{{1, 2, 3}, {null, 4, 5}, {6, 7, 8}}, 0, 1)))
             return true;
         return false;
     }
 
     @Override
-    protected Collection<Node<Integer[][], String>> Expand(Node<Integer[][], String> node) {
+    protected Collection<Node<Matrix, String>> Expand(Node<Matrix, String> node) {
 
     }
 }
