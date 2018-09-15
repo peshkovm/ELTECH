61cfe83e7c41aac9a2ab70c1d7f56e5401864f84
Denis Peshkov <denis_maksim@mail.ru>
2018-09-15 17:34:37 +0300

    Node - done!

--- AI_labs/src/com/eltech/common/Node.java
+++ AI_labs/src/com/eltech/common/Node.java
@@ -1,17 +1,22 @@
 package com.eltech.common;
 
 public class Node<S, A> {
+
+    private S State;
+    private A Action;
+    private Node<S, A> Parent;
+    private int Path_Cost;
+
+    public Node(S state) {
+        this(state,null,null,0);
+    }
+
     public Node(S state, A action, Node<S, A> parent, int path_cost) {
         State = state;
         Action = action;
         Parent = parent;
         Path_Cost = path_cost;
     }
-
-    private S State;
-    private A Action;
-    private Node<S, A> Parent;
-    private int Path_Cost;
 
     public S getState() {
         return State;
@@ -27,5 +32,10 @@ public class Node<S, A> {
 
     public int getPath_Cost() {
         return Path_Cost;
+    }
+
+    @Override
+    public String toString() {
+        return "State " + State;
     }
 }
