4b5bfcc9471754127d842af8d5a00f413efe91ab
Denis Peshkov <denis_maksim@mail.ru>
2019-02-26 00:11:27 +0300

    mpi program done

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -3,19 +3,34 @@
 
 using namespace std;
 
+long fibonachi(int n) {
+    int prev1 = 0, prev2 = 1;
+    int a = prev1;
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    for (int i = 1; i < n; i++) {
+        a = prev1 + prev2;
+        prev1 = prev2;
+        prev2 = a;
+    }
+    return a;
+}
+
 int main(int argc, char **argv) {
     MPI_Init(&argc, &argv);
-    int rank, proc_count, received_rank;
+    int rank, proc_count, res_;
     MPI_Comm_size(MPI_COMM_WORLD, &proc_count);
 
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     if (rank == 0) {
-        for (int i = 0; i < proc_count; i++) {
-            MPI_Recv(&received_rank, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            cout << received_rank << endl;
+        for (int i = 1; i < proc_count; i++) {
+            MPI_Recv(&res_, 1, MPI_INT, i, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            cout << "res " << res_ << endl;
         }
     } else {
-        MPI_Send(&rank, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+        long res = fibonachi(rank);
+        MPI_Send(&res, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
     }
+    MPI_Finalize();
     return 0;
 }
