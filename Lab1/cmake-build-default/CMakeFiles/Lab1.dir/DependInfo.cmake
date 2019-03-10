1db53c5c16e85a7691676110fe6005b67a31342c
Denis Peshkov <denis_maksim@mail.ru>
2019-03-10 16:15:30 +0300

    Lab1

---
+++ Lab1/cmake-build-default/CMakeFiles/Lab1.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/denis/Desktop/ELTECH/Lab1/main.cpp" "/home/denis/Desktop/ELTECH/Lab1/cmake-build-default/CMakeFiles/Lab1.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/mpich"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -5,9 +5,8 @@ using namespace std;
 
 long fibonachi(int n) {
     int prev1 = 0, prev2 = 1;
-    int a = prev1;
+    int a = 1;
     if (n == 0) return 0;
-    if (n == 1) return 1;
     for (int i = 1; i < n; i++) {
         a = prev1 + prev2;
         prev1 = prev2;
@@ -17,19 +16,21 @@ long fibonachi(int n) {
 }
 
 int main(int argc, char **argv) {
-    MPI_Init(&argc, &argv);
-    int rank, proc_count, res_;
+    MPI_Init(&argc,&argv);
+    int rank, proc_count;
+    long res_;
     MPI_Comm_size(MPI_COMM_WORLD, &proc_count);
 
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     if (rank == 0) {
         for (int i = 1; i < proc_count; i++) {
-            MPI_Recv(&res_, 1, MPI_INT, i, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Recv(&res_, i, MPI_LONG, i, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
             cout << "res " << res_ << endl;
         }
     } else {
         long res = fibonachi(rank);
-        MPI_Send(&res, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+        MPI_Send(&res, 1, MPI_LONG, 0, 0, MPI_COMM_WORLD);
     }
     MPI_Finalize();
     return 0;
